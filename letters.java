4925f85f35940a3345b38b7c93cb895750be0c1a
Nikita Mehtre <87356338+nikitamehtre@users.noreply.github.com>
2022-01-21 17:02:12 +0530

    Add files via upload

---
+++ letters.java
@@ -0,0 +1,34 @@
+//Program to check entered alphabet is Vowel or Consonant
+package alphabets;
+
+import java.util.Scanner;
+
+public class letters {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter an aplphabet ");
+		Scanner sc = new Scanner(System.in);
+		char letter = sc.next().charAt(0);
+		switch (letter)
+        {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'i':
+            case 'I':
+            case 'o':
+            case 'O':
+            case 'u':
+            case 'U':
+                System.out.println(letter + " Letter is vowel");
+                break;
+            default:
+                System.out.println(letter + " Letter is consonant");
+                break;
+		}
+	}
+}
+			
+		
